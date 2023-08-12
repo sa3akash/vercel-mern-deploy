41ef3af5b9e9941cadcad15eb7dd9ab9e12def49
sa3akash <sa3shakil@gmail.com>
2023-08-12 15:13:41 +0600

    somthing change

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import { useState } from "react";
 export default function App() {
 
   const [data,setData] = useState()
-
+  const [loading,setLoading] = useState(false)
   const fetchData = async () => {
+    setLoading(true)
     const res = await fetch(`${import.meta.env.VITE_API_URL}/test`)
     const json = await res.json();
     setData(json)
-
+    setLoading(false)
     
   }
 
@@ -17,7 +18,7 @@ export default function App() {
     <div>
     <h1>Test Site</h1>
       <h2>{data?.message}</h2>
-      <button onClick={fetchData}>get data</button>
+      <button onClick={fetchData} disabled={loading} style={{cursor: loading? "not-allowed": "pointer"}}>get data</button>
     </div>
   );
 }
